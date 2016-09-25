b4a7dea598d2d2418dcc332bba086ae3154f6bbf
eryw <kristeryw@gmail.com>
2016-09-25 18:37:02 +0700

    Remove duplicate logic
    The CakePHP core already remove duplicate logic in
    ControllerFactoryFilter. So, I should follow them.

--- src/Routing/Filter/ControllerFactoryFilter.php
+++ src/Routing/Filter/ControllerFactoryFilter.php
@@ -1,67 +1,29 @@
 <?php
 namespace PastryBag\Routing\Filter;
 
-use Cake\Core\App;
-use Cake\Routing\Filter\ControllerFactoryFilter as DefaultControllerFactory;
-use Cake\Utility\Inflector;
-use PastryBag\Di\PastryBag;
+use PastryBag\Http\ControllerFactory;
+use Cake\Routing\Filter\ControllerFactoryFilter as CakeControllerFactoryFilter;
 
-class ControllerFactoryFilter extends DefaultControllerFactory
+/**
+ * A dispatcher filter that builds the controller to dispatch
+ * in the request.
+ *
+ * This filter resolves the request parameters into a controller
+ * instance and attaches it to the event object.
+ */
+class ControllerFactoryFilter extends CakeControllerFactoryFilter
 {
-
     /**
-     * {@inheritdoc}
+     * Gets controller to use, either plugin or application controller.
      *
-     * @param \Cake\Network\Request $request
-     * @param \Cake\Network\Response $response
-     * @return bool|object
+     * @param \Cake\Network\Request $request Request object
+     * @param \Cake\Network\Response $response Response for the controller.
+     * @return \Cake\Controller\Controller
      */
     protected function _getController($request, $response)
     {
-        $pluginPath = $controller = null;
-        $namespace = 'Controller';
-        if (!empty($request->params['plugin'])) {
-            $pluginPath = $request->params['plugin'] . '.';
-        }
-        if (!empty($request->params['controller'])) {
-            $controller = $request->params['controller'];
-        }
-        if (!empty($request->params['prefix'])) {
-            if (strpos($request->params['prefix'], '/') === false) {
-                $namespace .= '/' . Inflector::camelize($request->params['prefix']);
-            } else {
-                $prefixes = array_map(
-                    'Cake\Utility\Inflector::camelize',
-                    explode('/', $request->params['prefix'])
-                );
-                $namespace .= '/' . implode('/', $prefixes);
-            }
-        }
-        $firstChar = substr($controller, 0, 1);
-        if (strpos($controller, '\\') !== false ||
-            strpos($controller, '/') !== false ||
-            strpos($controller, '.') !== false ||
-            $firstChar === strtolower($firstChar)
-        ) {
-            return false;
-        }
-        $className = false;
-        if ($pluginPath . $controller) {
-            $className = App::classname($pluginPath . $controller, $namespace, 'Controller');
-        }
-        if (!$className) {
-            return false;
-        }
+        $factory = new ControllerFactory();
 
-        $di = PastryBag::container();
-        if (isset(class_parents($className)['PastryBag\Controller\Controller'])) {
-            $instance = $di->newInstance($className);
-            $instance->construct($request, $response, $controller);
-        } else {
-            $di->params[$className] = [$request, $response, $controller];
-            $instance = $di->newInstance($className);
-        }
-
-        return $instance;
+        return $factory->create($request, $response);
     }
 }
